5da6590886300ef3b2adb95aef67e07b4671e6ba
lasthor <hourabdo89@gmail.com>
2023-10-16 10:39:56 +0100

    remove some not nessring things

--- src/components/Section_01.js
+++ src/components/Section_01.js
@@ -3,9 +3,9 @@ import videsection from '../vids/committement--.mp4'
 const section_01 = () => {
   return (
     <section className="section_01">
-      <div></div>
+      
       <video src={videsection} autoPlay loop muted className='video'/>
-      <div></div>
+      
 
     </section>
   )
